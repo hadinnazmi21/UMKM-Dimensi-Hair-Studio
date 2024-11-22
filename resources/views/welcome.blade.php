760875cd1ba3b8a74c8e1b9a34606150c6c8fa29
Sabilr12 <sabil23ti@mahasiswa.pcr.ac.id>
2024-11-22 10:59:20 +0700

    sabil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>UMKM Dimensi Hair Studio aja</title>
+        <title>UMKM Dimensi Hair Studio</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
